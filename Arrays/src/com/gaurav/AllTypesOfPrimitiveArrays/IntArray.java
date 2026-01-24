2773bb31cc66383186d5eeeca4cb057fb18b8601
Gaurav Patil <gp949958@gmail.com>
2026-01-24 14:49:51 +0530

    added more primitive arrays

---
+++ Arrays/src/com/gaurav/AllTypesOfPrimitiveArrays/IntArray.java
@@ -0,0 +1,16 @@
+package com.gaurav.alltypesofprimitivearrays;
+
+public class IntArray 
+{
+	public static void main(String[] args) 
+	{
+		int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+		System.out.println("Integer Array Elements:");
+
+		for(int i : arr) 
+		{
+			System.out.print(i + "\t");
+		}
+	}
+}
---
+++ Arrays/src/com/gaurav/AllTypesOfPrimitiveArrays/LongArray.java
@@ -0,0 +1,16 @@
+package com.gaurav.alltypesofprimitivearrays;
+
+public class LongArray 
+{
+	public static void main(String[] args) 
+	{
+		long arr[] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+
+		System.out.println("Long Array Elements:");
+
+		for(long i : arr) 
+		{
+			System.out.print(i + "\t");
+		}
+	}
+}
