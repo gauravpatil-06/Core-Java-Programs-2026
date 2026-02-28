0af10ff912bf85d8ab027e89b99f99bdb6fcaf35
Gaurav Patil <gp949958@gmail.com>
2026-02-28 11:20:45 +0530

    added arraylist types

---
+++ Collection/src/com/gaurav/arraylist/ByteArrayList.java
@@ -0,0 +1,25 @@
+package com.gaurav.arraylist;
+
+import java.util.ArrayList;
+
+public class ByteArrayList 
+{
+    public static void main(String[] args) 
+    {
+
+        ArrayList<Byte> al = new ArrayList<Byte>();
+
+        al.add((byte) 1);
+        al.add((byte) 2);
+        al.add((byte) 3);
+        al.add((byte) 4);
+        al.add((byte) 5);
+        al.add((byte) 6);
+        al.add((byte) 7);
+        al.add((byte) 8);
+        al.add((byte) 9);
+        al.add((byte) 10);
+
+        System.out.println(al);
+    }
+}
---
+++ Collection/src/com/gaurav/arraylist/CharacterArrayList.java
@@ -0,0 +1,24 @@
+package com.gaurav.arraylist;
+
+import java.util.ArrayList;
+
+public class CharacterArrayList 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<Character> al = new ArrayList<Character>();
+		
+		al.add('A');
+		al.add('B');
+		al.add('C');
+		al.add('D');
+		al.add('E');
+		al.add('F');
+		al.add('G');
+		al.add('H');
+		al.add('I');
+		al.add('J');
+		
+		System.out.println(al);
+	}
+}
