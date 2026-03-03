4ddc01e2dd35145e6a343cbc2a7387182b05cc3b
Gaurav Patil <gp949958@gmail.com>
2026-03-03 18:10:20 +0530

    added string arraylist

---
+++ Collection/src/com/gaurav/arraylist/StringArrayList.java
@@ -0,0 +1,24 @@
+package com.gaurav.arraylist;
+
+import java.util.ArrayList;
+
+public class StringArrayList 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<String> al = new ArrayList<String>();
+		
+		al.add("Gaurav");
+		al.add("Vinod");
+		al.add("Bhagyesh");
+		al.add("Suraj");
+		al.add("Vikas");
+		al.add("Abhishek");
+		al.add("Vitthal");
+		al.add("Akash");
+		al.add("Praful");
+		al.add("Pavan");
+		
+		System.out.println(al);
+	}
+}
