67eee500ad33adf874561894d894c8a0cf79b865
Gaurav Patil <gp949958@gmail.com>
2026-03-11 14:00:00 +0530

    added more linkedlist types

---
+++ Collection/src/com/gaurav/linkedlist/DoubleLinkedList.java
@@ -0,0 +1,24 @@
+package com.gaurav.linkedlist;
+
+import java.util.LinkedList;
+
+public class DoubleLinkedList
+{
+    public static void main(String[] args)
+    {
+        LinkedList<Double> ll = new LinkedList<Double>();
+
+        ll.add(100.50);
+        ll.add(200.50);
+        ll.add(300.50);
+        ll.add(400.50);
+        ll.add(500.50);
+        ll.add(600.50);
+        ll.add(700.50);
+        ll.add(800.50);
+        ll.add(900.50);
+        ll.add(1000.50);
+
+        System.out.println(ll);
+    }
+}
