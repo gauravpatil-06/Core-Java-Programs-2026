5099f0af87e3b27de3ed7c9dd8fdb54709b93378
Gaurav Patil <gp949958@gmail.com>
2026-03-23 12:00:00 +0530

    added product vector

---
+++ Collection/src/com/gaurav/vector/EmployeeVector.java
@@ -0,0 +1,25 @@
+package com.gaurav.vector;
+
+import java.util.Vector;
+
+public class EmployeeVector 
+{
+	public static void main(String[] args) 
+	{
+		Vector <Employee> emp = new Vector <Employee>();
+		
+		emp.add(new Employee(101, "Gaurav", 100000.50, "IT", "Pune"));
+		emp.add(new Employee(102, "Vinod", 88000.00, "IT", "Mumbai"));
+		emp.add(new Employee(103, "Suraj", 60000.00, "Developer", "Nashik"));
+		emp.add(new Employee(104, "Akash", 59000.00, "Testing", "Nagpur"));
+		emp.add(new Employee(105, "Abhishek", 48000.25, "Support", "Kolhapur"));
+		
+		for (Employee e : emp) 
+		{
+			if (e.getSalary() > 60000.00  && e.getDepartment().equals("IT"))
+			{
+				e.display();
+			}
+		}
+	}
+}
